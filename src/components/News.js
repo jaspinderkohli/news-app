1d05028097e4793d5dbbdc4022e7e7522ee3942a
Jaspinder Singh <jaspinderkohli@gmail.com>
2024-03-17 23:34:20 -0230

    pageination added

--- src/components/News.js
+++ src/components/News.js
@@ -1,46 +1,100 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
-     articles =  []
-    constructor() {
-        super();
-        console.log("Hello I am a constructor from News component");
-        this.state = {
-            articles : this.articles,
-            loading: false
-        }
-    }
+  constructor() {
+    console.log("Hello I am a constructor from News component");
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      pageSize: 12,
+    };
+  }
 
-    // called after the render method
-    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc"
-        // let url = "https://newsapi.org/v2/everything?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc"
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({articles: parsedData.articles})
-    }
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url =
+      `https://newsapi.org/v2/everything?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc&pageSize=${this.state.pageSize}` +
+      `&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("Next");
+    let url =
+    `https://newsapi.org/v2/everything?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc&pageSize=${this.state.pageSize}` +
+    `&page=${this.state.page + 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+    page: this.state.page + 1,
+    articles: parsedData.articles,
+    });
+  };
+
+  // called after the render method
+  async componentDidMount() {
+    // let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc"
+    let url =
+      "https://newsapi.org/v2/everything?sources=techcrunch&apiKey=6bd6166654804b34a8fdc039e264fadc&pageSize=12&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
   render() {
     return (
       <div className="container my-3">
-        <h2> NewsWave - Top Tech Headlines</h2>
+        <h1> NewsWave - Top Tech Headlines</h1>
         <div className="row">
-            {
-            this.state.articles.map(
-                (element)=>
-                {
-                    return <div className="col md-4" key={element.url}>
-                                {/* <NewsItems title={(element.title?element.title:"").slice(0, 45)} description={(element.description?element.description:"").slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/> */}
-                                <NewsItems title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
-                           </div>
-                }
-              )
-            };
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col md-4" key={element.url}>
+                {/* <NewsItems title={(element.title?element.title:"").slice(0, 45)} description={(element.description?element.description:"").slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/> */}
+                <NewsItems
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+          ;
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.state.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
